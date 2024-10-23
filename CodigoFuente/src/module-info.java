cae4f844ae0ad5de2ee9a655b18d2c87e3da8e94
Agustin Paul <a.agustinpaul@gmail.com>
2024-10-23 16:38:26 -0300

    Modificando el .gitigore

--- CodigoFuente/src/module-info.java
+++ CodigoFuente/src/module-info.java
@@ -4,16 +4,6 @@
 /**
  * 
  */
-module superMario {
-	exports elementos;
-	exports fabricas;
-	exports generadores;
-	exports imagenes;
-	exports juego;
-	exports launcher;
-	exports observers;
-	exports ventanas;
-	exports visitors;
-	exports vista;
+module CodigoFuente {
 	requires java.desktop;
 }
