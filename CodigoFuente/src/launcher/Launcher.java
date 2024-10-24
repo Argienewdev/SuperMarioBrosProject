6e93d708673ecb9ecc83e5bc0c1bce472c7368b6
Martin Dupuy <dupuymartin7@gmail.com>
2024-10-24 11:56:36 -0300

    Moviendo main al launcher

--- CodigoFuente/src/launcher/Launcher.java
+++ CodigoFuente/src/launcher/Launcher.java
@@ -1,5 +1,17 @@
 package launcher;
 
-public class Launcher {
+import juego.BucleJuego;
+import juego.Juego;
+import sonido.ReproductorDeMusicaFondo;
+import ventanas.ControladorVistas;
 
+public class Launcher {
+    public static void main(String[] args) {
+    	 Juego juego = new Juego();
+         juego.establecerControladorVistas(new ControladorVistas(juego));
+         new BucleJuego(juego);
+        ReproductorDeMusicaFondo reproductorDeMusicaFondo = new ReproductorDeMusicaFondo();
+        reproductorDeMusicaFondo.playMusic("src/sonido/musica_juego.wav");
+        // Aquí puedes iniciar la partida o realizar otras configuraciones
+    }
 }
