86cc8d6095d0f7ebdfaa9d4640d32dedfdb83f13
Emiliano Sensini <emilianosensini@gmail.com>
2024-10-30 20:29:25 -0300

    Pequeños cambios para mejorar la consistencia del codigo

--- CodigoFuente/src/fabricas/FabricaPlataformas.java
+++ CodigoFuente/src/fabricas/FabricaPlataformas.java
@@ -69,8 +69,7 @@ public class FabricaPlataformas {
 		Sprite spriteTuberia = this.fabricaSprites.obtenerTuberia(altura);
 		int ancho = 100;
 		int alturaEscalada = altura * 50;
-		PiranhaPlant piranhaPlant = null;
-		Tuberia tuberiaADevolver = new Tuberia(spriteTuberia, posicion, null, null, piranhaPlant, alturaEscalada, ancho);
+		Tuberia tuberiaADevolver = new Tuberia(spriteTuberia, posicion, null, null, alturaEscalada, ancho);
         Visitante visitor = new VisitorTuberia(tuberiaADevolver);
         tuberiaADevolver.establecerVisitor(visitor);
 		ObserverGrafico observerGraficoTuberia = new ObserverGrafico(tuberiaADevolver);
@@ -85,8 +84,7 @@ public class FabricaPlataformas {
 		Sprite spriteTuberia = this.fabricaSprites.obtenerTuberia(altura);
 		int ancho = 100;
   		int alturaEscalada = altura*50;
-  		PiranhaPlant piranhaPlant= null;
-		Tuberia tuberiaADevolver = new Tuberia(spriteTuberia, posicion, null, null, piranhaPlant, alturaEscalada, ancho);
+		Tuberia tuberiaADevolver = new Tuberia(spriteTuberia, posicion, null, null, alturaEscalada, ancho);
         Visitante visitorTuberia = new VisitorTuberia(tuberiaADevolver);
         tuberiaADevolver.establecerVisitor(visitorTuberia);
 		ObserverGrafico observerGraficoTuberia = new ObserverGrafico(tuberiaADevolver);
@@ -95,8 +93,7 @@ public class FabricaPlataformas {
         int posicionPiranhaX = posicion.x+25;
         int posicionPiranhaY = posicion.y -spriteTuberia.obtenerAltoImagen()+150;
        	Point posicionPiranha = new Point(posicionPiranhaX,posicionPiranhaY);
-       	piranhaPlant = this.fabricaEntidades.obtenerPiranhaPlant(posicionPiranha);
-       	tuberiaADevolver.establecerPiranha(piranhaPlant);
+       	PiranhaPlant piranhaPlant = this.fabricaEntidades.obtenerPiranhaPlant(posicionPiranha);
        	nivel.agregarEnemigo(piranhaPlant);
 		return tuberiaADevolver;
 	}
@@ -134,9 +131,8 @@ public class FabricaPlataformas {
 		ObserverGrafico observerGraficoBloqueDePregunta = new ObserverGrafico(bloqueDePreguntaADevolver);	  
 		bloqueDePreguntaADevolver.establecerObserverGrafico(observerGraficoBloqueDePregunta);
 		Random random = new Random();
-		//int identificadorPowerUp = random.nextInt(8) + 1; // Genera un número entre 1 (inclusive) y 8 (inclusive)
-		int identificadorPowerUp = 8;
-		
+		int identificadorPowerUp = random.nextInt(8) + 1; // Genera un número entre 1 (inclusive) y 8 (inclusive)
+		//int identificadorPowerUp = 7;
 		switch(identificadorPowerUp) {
 			case 2,4,6,8: {
 				for(int contador = 1; contador <= identificadorPowerUp; contador++) {
@@ -150,7 +146,6 @@ public class FabricaPlataformas {
 			case 1: {
 				Estrella estrella = this.fabricaEntidades.obtenerEstrella(posicion);
 				nivel.agregarPowerUp(estrella);
-		        pantallaDeJuego.agregarLabel(estrella.obtenerObserverGrafico());
 		        bloqueDePreguntaADevolver.agregarPowerUp(estrella);
 		        estrella.establecerBloquePregunta(bloqueDePreguntaADevolver);
 		        break;
@@ -158,7 +153,6 @@ public class FabricaPlataformas {
 			case 3: {
 				ChampinionVerde champinionVerde = this.fabricaEntidades.obtenerChampinionVerde(posicion);
 		        nivel.agregarPowerUp(champinionVerde);
-		        pantallaDeJuego.agregarLabel(champinionVerde.obtenerObserverGrafico());
 		        bloqueDePreguntaADevolver.agregarPowerUp(champinionVerde);
 		        champinionVerde.establecerBloquePregunta(bloqueDePreguntaADevolver);
 		        break;
@@ -166,7 +160,6 @@ public class FabricaPlataformas {
 			case 5: {
 				FlorDeFuego florDeFuego = this.fabricaEntidades.obtenerFlorDeFuego(posicion,fabricaEntidades);
 		        nivel.agregarPowerUp(florDeFuego);
-		        pantallaDeJuego.agregarLabel(florDeFuego.obtenerObserverGrafico());
 		        bloqueDePreguntaADevolver.agregarPowerUp(florDeFuego);
 		        florDeFuego.establecerBloquePregunta(bloqueDePreguntaADevolver);
 		        break;
@@ -174,7 +167,6 @@ public class FabricaPlataformas {
 			case 7: {
 				SuperChampinion superChampinion = this.fabricaEntidades.obtenerSuperChampinion(posicion);
 		        nivel.agregarPowerUp(superChampinion);
-		        pantallaDeJuego.agregarLabel(superChampinion.obtenerObserverGrafico());
 		        bloqueDePreguntaADevolver.agregarPowerUp(superChampinion);
 		        superChampinion.establecerBloquePregunta(bloqueDePreguntaADevolver);
 		        break;
