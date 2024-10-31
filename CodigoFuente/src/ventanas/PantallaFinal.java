17ab0f7a8de9ecb226dcbd89b507c068afd90501
Martin Dupuy <dupuymartin7@gmail.com>
2024-10-31 13:17:12 -0300

    Eliminacion boton cerrar juego y generalizacion a Pantalla

--- CodigoFuente/src/ventanas/PantallaFinal.java
+++ CodigoFuente/src/ventanas/PantallaFinal.java
@@ -25,8 +25,6 @@ public class PantallaFinal extends Pantalla {
 
     protected JButton botonVolver;
     
-    protected JButton botonCerrar;
-    
     private JLabel puntajeLabel;
     
     private JLayeredPane panelCapas;
@@ -54,9 +52,7 @@ public class PantallaFinal extends Pantalla {
         panelCapas.setLayout(null);
         panelCapas.setBounds(0, 0, size.width, size.height);
 
-        // Reemplazar JLabel por JButton
         botonVolver = new JButton("Pantalla inicial");
-        botonCerrar = new JButton("Cerrar juego");
 
         configurarFuente();
         establecerFondo();
@@ -70,11 +66,7 @@ public class PantallaFinal extends Pantalla {
     private void realizarAccionVolver() {
         controlador.dePantallaFinalAPantallaInicial();
     }
-    
-    private void realizarAccionCerrar() {
-        System.exit(0);
-    }
-    
+        
     private void establecerFondo() {
        JLabel fondo = new JLabel();
        fondo.setBackground(Color.BLACK);
@@ -87,7 +79,6 @@ public class PantallaFinal extends Pantalla {
     private void configurarFuente() {
         tipoFuentes = new Fuentes();
         botonVolver.setFont(tipoFuentes.fuente(tipoFuentes.pxl, 0, ConstantesGlobales.PANEL_ANCHO / 40));
-        botonCerrar.setFont(tipoFuentes.fuente(tipoFuentes.pxl, 0, ConstantesGlobales.PANEL_ANCHO / 40));
     }
     
     private void establecerMensaje() {
@@ -113,7 +104,6 @@ public class PantallaFinal extends Pantalla {
 
     private void configurarBotones() {
         int alturaPantalla = size.height;
-        int espaciadoBotones = 60; 
         int posicionPrimerBotonY = (2 * alturaPantalla / 3) + 50; 
         
         botonVolver.setBounds(
@@ -123,39 +113,20 @@ public class PantallaFinal extends Pantalla {
             botonVolver.getPreferredSize().height
         );
         
-        botonCerrar.setBounds(
-            (size.width - botonCerrar.getPreferredSize().width) / 2, 
-            posicionPrimerBotonY + espaciadoBotones,
-            botonCerrar.getPreferredSize().width + 20, 
-            botonCerrar.getPreferredSize().height
-        );
-        
         botonVolver.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 realizarAccionVolver();
             }
         });
 
-        botonCerrar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                realizarAccionCerrar();
-            }
-        });
-        
         botonVolver.setBorderPainted(false);       
         botonVolver.setContentAreaFilled(false);    
         botonVolver.setFocusPainted(false);        
         botonVolver.setOpaque(false);               
         botonVolver.setForeground(Color.WHITE);
         
-        botonCerrar.setBorderPainted(false);       
-        botonCerrar.setContentAreaFilled(false);    
-        botonCerrar.setFocusPainted(false);        
-        botonCerrar.setOpaque(false);               
-        botonCerrar.setForeground(Color.WHITE);
 
         add(botonVolver);
-        add(botonCerrar);
     }
     
     private void configurarMapasTeclado() {
@@ -168,17 +139,9 @@ public class PantallaFinal extends Pantalla {
                 realizarAccionVolver();
             }
         });
-
-        inputMap.put(KeyStroke.getKeyStroke("ESCAPE"), "cerrar");
-        actionMap.put("cerrar", new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                realizarAccionCerrar();
-            }
-        });
     }
     
     public void refrescar() {
-    	establecerEnFoco(true);
     }
     
     public void establecerEnFoco (boolean foco) {
