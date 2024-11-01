0c6492bf19d2ee43bba4984d7382f5cc9ebbd6bc
Santiago Salamanca <salamancansantiago_2004@outlook.com>
2024-11-01 19:41:45 -0300

    Merge

--- CodigoFuente/src/fabricas/FabricaPlataformas.java
+++ CodigoFuente/src/fabricas/FabricaPlataformas.java
@@ -133,9 +133,8 @@ public class FabricaPlataformas {
 		ObserverGrafico observerGraficoBloqueDePregunta = new ObserverGrafico(bloqueDePreguntaADevolver);	  
 		bloqueDePreguntaADevolver.establecerObserverGrafico(observerGraficoBloqueDePregunta);
 		Random random = new Random();
-		//int identificadorPowerUp = random.nextInt(8) + 1; // Genera un número entre 1 (inclusive) y 8 (inclusive)
+		int identificadorPowerUp = random.nextInt(8) + 1; // Genera un número entre 1 (inclusive) y 8 (inclusive)
 
-		int identificadorPowerUp= 5;
 		switch(identificadorPowerUp) {
 			case 2,4,6,8: {
 				for(int contador = 1; contador <= identificadorPowerUp; contador++) {
